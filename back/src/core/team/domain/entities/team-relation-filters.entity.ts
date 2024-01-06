76c34c379b02c7fca52758a92d58328cb5f5480d
tommarto <tpmarto@gmail.com>
2024-01-06 11:51:11 -0300

    player property of TeamRelationFilters support nested relations

--- back/src/core/team/domain/entities/team-relation-filters.entity.ts
+++ back/src/core/team/domain/entities/team-relation-filters.entity.ts
@@ -1,11 +1,11 @@
 export class TeamRelationFilters {
   venues?: boolean;
-  players?: boolean;
+  players?: boolean | {details: boolean | {type: boolean}};
   homeMatches?: boolean;
   awayMatches?: boolean;
   coaches?: boolean;
 
-  constructor(venues?: boolean, players?: boolean, homeMatches?: boolean, awayMatches?: boolean, coaches?: boolean) {
+  constructor(venues?: boolean, players?: boolean | {details: boolean | {type: boolean}}, homeMatches?: boolean, awayMatches?: boolean, coaches?: boolean) {
     this.venues = venues;
     this.players = players;
     this.homeMatches = homeMatches;
